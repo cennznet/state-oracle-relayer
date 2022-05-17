dc7dcff755edac9e82df8b4180d8f2098f3f92e0
KarishmaBothara <bothara.karishma@gmail.com>
2022-05-17 14:07:42 +1200

    script to deploy state oracle demo contract to CENNZnet chain

---
+++ libs/scripts/deploy.ts
@@ -0,0 +1,24 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+
+    // We get the contract to deploy
+    const StateOracle = await ethers.getContractFactory("StateOracle");
+    const oracle = await StateOracle.deploy();
+
+    await oracle.deployed();
+
+    console.log("State oracle deployed to:", oracle.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});

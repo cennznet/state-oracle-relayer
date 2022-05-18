c5ce2c050cdd56c701fee87bdbf6c351bff0c24a
Ken Vu <ken.vu@centrality.ai>
2022-05-18 14:23:32 +1200

    Setup winston logging

--- commands/request-listener.ts
+++ commands/request-listener.ts
@@ -1,11 +1,18 @@
+import { getLogger } from "@/libs/utils/getLogger";
 import { getRabbitMQSet } from "@/libs/utils/getRabbitMQSet";
 import { AMQPError } from "@cloudamqp/amqp-client";
 
+const logger = getLogger("RequestListener");
+
 (async function run() {
 	try {
-		const [channel, queue] = await getRabbitMQSet();
-		queue.publish("hello world");
-		await channel.close();
+		logger.info(`
+State 🔮 Relayer
+	Signer: ABC
+	CENNZnet: Rata
+	Ethereum: Ropsten
+		`);
+
 		process.exit(0);
 	} catch (error: any) {
 		if (error instanceof AMQPError) error?.connection?.close();
---
+++ libs/utils/getLogger.ts
@@ -0,0 +1,40 @@
+import { createLogger, format, transports, Logger } from "winston";
+
+type LoggerService = "RequestListener" | "RequestProccessor";
+const instances = {} as Record<LoggerService, Logger>;
+
+export const getLogger = (service: LoggerService): Logger => {
+	if (instances[service]) return instances[service];
+
+	return (instances[service] = createLogger({
+		level: "info",
+		format: format.combine(
+			format.label({
+				label: service,
+				message: true,
+			}),
+			format.timestamp({
+				format: "YYYY-MM-DD HH:mm:ss",
+			}),
+			format.errors({ stack: true }),
+			format.splat(),
+			format.json()
+		),
+		transports: [
+			new transports.File({
+				filename: `logs/app.log`,
+				maxsize: 2048000, // 2 MB
+				maxFiles: 10,
+			}),
+
+			new transports.Console({
+				format: format.combine(
+					format.colorize(),
+					format.printf(({ level, message, timestamp }) => {
+						return `${timestamp} ${level}: ${message}`;
+					})
+				),
+			}),
+		],
+	}));
+};

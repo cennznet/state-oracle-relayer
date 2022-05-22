ecd3bbb084411510faa20b2f684754561df5df40
Ken Vu <ken.vu@centrality.ai>
2022-05-23 11:14:24 +1200

    Provide `alchemy` and `infura` API details

--- libs/utils/getEthersProvider.ts
+++ libs/utils/getEthersProvider.ts
@@ -1,6 +1,13 @@
-import { ETHEREUM_NETWORK } from "@/libs/constants";
+import {
+	ALCHEMY_API_TOKEN,
+	ETHEREUM_NETWORK,
+	INFURA_PROJECT,
+} from "@/libs/constants";
 import { ethers } from "ethers";
 
 export const getEthersProvider = (): ethers.providers.BaseProvider => {
-	return ethers.getDefaultProvider(ETHEREUM_NETWORK);
+	return ethers.getDefaultProvider(ETHEREUM_NETWORK, {
+		alchemy: ALCHEMY_API_TOKEN,
+		infura: INFURA_PROJECT,
+	});
 };

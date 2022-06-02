6356189f0e7cdf783f9133bf2fc0aa2fa908ef68
Ken Vu <ken.vu@centrality.ai>
2022-06-02 14:57:03 +1200

    Update default ETH network to be Ropsten

--- libs/constants.ts
+++ libs/constants.ts
@@ -5,7 +5,8 @@ export const MONGODB_SERVER: string =
 export const RABBBITMQ_SERVER: string =
 	process.env.RABBBITMQ_SERVER ?? "amqp://guest:guest@localhost:5672";
 export const CENNZNET_NETWORK: string = process.env.CENNZNET_NETWORK ?? "local";
-export const ETHEREUM_NETWORK: string = process.env.ETHEREUM_NETWORK ?? "kovan";
+export const ETHEREUM_NETWORK: string =
+	process.env.ETHEREUM_NETWORK ?? "ropsten";
 export const CENNZNET_SIGNER: string = process.env.CENNZNET_SIGNER ?? "";
 export const MESSAGE_MAX_RETRY: number = Number(
 	process.env.MESSAGE_MAX_RETRY ?? 3
